b0d8c148709a73c8b1edb3ff508410abd318e565
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-05-25 15:28:23 -0600

    add a new proyect Calculator

---
+++ js/04.js
@@ -0,0 +1,14 @@
+// Strings
+
+// in the follow
+const tweet = "im learning js whit a Juan CODE master";
+
+console.log(tweet);
+
+
+// numbers
+
+const num1 = 100;
+const num2 = 200;
+
+console.log(num1)
---
+++ js/10.js
@@ -0,0 +1,11 @@
+// objects;
+// to define a new object:
+
+const product = {
+    // to define a new property
+    nameProduct : "Gamer Monitor",
+    price : 300,
+    available : true
+}
+
+console.log(product);
