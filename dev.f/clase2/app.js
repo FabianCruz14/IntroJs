d1993ad1111b13d0585fa17f0dab59ede2a6c97e
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-08-17 12:21:04 -0600

    adding a class 3 from devf

---
+++ dev.f/clase2/app.js
@@ -0,0 +1,2 @@
+// first, we created a functions to get information from a index
+const taskInput = document.getElementById("taskInput")
---
+++ dev.f/clase3/app.js
@@ -0,0 +1,15 @@
+// promise
+// how to declarate a promise
+const Promise = new Promesa((resolve, reject)) = {
+    // codigo asincrono a resolver
+    // ejemplo; llamdas apis, db,
+    // leer archivos, etc
+    resolve(cosaQueDevuelveSiSeEjecutaBien);
+    reject(cosaQueDevuelveSiSeEjecutaMal);
+}
+
+// ejecucion de la promesa
+
+Promise
+
+    .then
