b51d23e2377616a2a0be5066de1950b9fda77f89
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-08-23 20:08:43 -0600

    app show a Batman info

--- dev.f/clase5/app.js
+++ dev.f/clase5/app.js
@@ -1,24 +1,37 @@
-//consumir la APO¡I de Rick & Morty y nos va a mostrar los caracteres
+//consumir la API de Superheroes y nos va a mostrar los caracteres
 const axios = require('axios')
 
 // constante con la url de la API
-const urlAPI = "https://superheroapi.com/api/3161326367507697/70"
+const urlAPI = "https://superheroapi.com/api/3161326367507697/70";
 
-// funcion para obtener y mostrar los datos
-async function getCharacteres() {
+async function inspectResponse() {
     try {
-        //codigo principal
-        const response = await axios.get(urlAPI)
-        const characters = response.data.results
-
-        console.log("SuperHeroes List")
-
-
-
-
-
-
+        const response = await axios.get(urlAPI);
+        console.log(response.data); // Imprime la respuesta completa en la consola
     } catch (error) {
-        // muestra un error
+        console.log("Error al obtener la información", error);
     }
 }
+inspectResponse();
+
+// funcion para obtener y mostrar los datos
+// async function getHeroes() {
+//     try {
+//         //codigo principal
+//         const response = await axios.get(urlAPI)
+//         const heroes = response.data
+
+//         console.log("SuperHeroes List")
+
+//         heroes.forEach(superhero => {
+//             console.log(`** ${superhero.name}`)
+//             console.log(`* ${superhero.publisher}`)
+
+//         });
+
+//     } catch (error) {
+//         // muestra un error
+//         console.log("Error al obtener la informacion", error)
+//     }
+// }
+// getHeroes();
