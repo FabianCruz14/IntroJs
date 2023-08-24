1b486d504a8d0878a2c428d5bca322fb5be26f44
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-08-23 22:58:01 -0600

    before a big changes

--- dev.f/clase5/app.js
+++ dev.f/clase5/app.js
@@ -4,34 +4,32 @@ const axios = require('axios')
 // constante con la url de la API
 const urlAPI = "https://superheroapi.com/api/3161326367507697/70";
 
-async function inspectResponse() {
-    try {
-        const response = await axios.get(urlAPI);
-        console.log(response.data); // Imprime la respuesta completa en la consola
-    } catch (error) {
-        console.log("Error al obtener la información", error);
-    }
-}
-inspectResponse();
-
-// funcion para obtener y mostrar los datos
-// async function getHeroes() {
+// async function inspectResponse() {
 //     try {
-//         //codigo principal
-//         const response = await axios.get(urlAPI)
-//         const heroes = response.data
-
-//         console.log("SuperHeroes List")
-
-//         heroes.forEach(superhero => {
-//             console.log(`** ${superhero.name}`)
-//             console.log(`* ${superhero.publisher}`)
-
-//         });
-
+//         const response = await axios.get(urlAPI);
+//         console.log(response.data); // Imprime la respuesta completa en la consola
 //     } catch (error) {
-//         // muestra un error
-//         console.log("Error al obtener la informacion", error)
+//         console.log("Error al obtener la información", error);
 //     }
 // }
-// getHeroes();
+// inspectResponse();
+
+// funcion para obtener y mostrar los datos
+async function getHeroes() {
+    try {
+        //codigo principal
+        const response = await axios.get(urlAPI)
+        const heroes = response.data
+
+        console.log("SuperHeroes List")
+        const name = superhero.name;
+
+
+    } catch (error) {
+        // muestra un error
+        console.log("Error al obtener la informacion", error)
+    }
+}
+getHeroes();
+
+
