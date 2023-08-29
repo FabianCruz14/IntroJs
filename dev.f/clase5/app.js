01ca2a6697195cc58def32ecd52290e3403aa30e
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-08-28 23:15:30 -0600

    adding a class6

--- dev.f/clase5/app.js
+++ dev.f/clase5/app.js
@@ -1,35 +1,22 @@
-//consumir la API de Superheroes y nos va a mostrar los caracteres
-const axios = require('axios')
+const axios = require('axios');
 
-// constante con la url de la API
 const urlAPI = "https://superheroapi.com/api/3161326367507697/70";
 
-// async function inspectResponse() {
-//     try {
-//         const response = await axios.get(urlAPI);
-//         console.log(response.data); // Imprime la respuesta completa en la consola
-//     } catch (error) {
-//         console.log("Error al obtener la información", error);
-//     }
-// }
-// inspectResponse();
-
-// funcion para obtener y mostrar los datos
 async function getHeroes() {
     try {
-        //codigo principal
-        const response = await axios.get(urlAPI)
-        const heroes = response.data
+        const response = await axios.get(urlAPI);
+        const heroes = response.data.results;
 
-        console.log("SuperHeroes List")
-        const name = superhero.name;
+        console.log("SuperHeroes List");
 
+        heroes.forEach(superhero => {
+            const name = superhero.name;
+            console.log(`** Name: ${name}`);
+        });
 
     } catch (error) {
-        // muestra un error
-        console.log("Error al obtener la informacion", error)
+        console.log("Error al obtener la información", error);
     }
 }
+
 getHeroes();
-
-
