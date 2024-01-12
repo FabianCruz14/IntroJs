32cb53d0d086e192effe2e636bddf812bb8e009a
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2024-01-11 18:54:55 -0600

    adidng a html file

---
+++ bookExercises/app.js
@@ -0,0 +1,5 @@
+var book = { //los objetos estan encerrados en llaves
+    topic: "javascript", //la propiedad "topic" tiene el valor de "javascript"
+    fat: true // la propiedad fat es verdadera
+};
+console.log(book.topic);
